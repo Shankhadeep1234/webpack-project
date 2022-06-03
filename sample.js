04152e8be902233452a50c0ca2854941da1bbedc
Shankhadeep1234 <shankhadeepraj@gmail.com>
2022-06-03 09:58:31 +0530

    array filter

--- sample.js
+++ sample.js
@@ -1,8 +1,17 @@
 //map.Array
-const myArray = [1, 2, 3, 4, 5];
+const myArrayMap = [1, 2, 3, 4, 5];
 
-const myNewArray = myArray.map((num) => {
+const myNewArrayMap = myArrayMap.map((num) => {
   return num + 1;
 });
 
-console.log(myNewArray);
+console.log(myNewArrayMap);
+
+//filter.Array
+const myArrayFilter = [1, 2, 3, 4, 5, 6];
+
+const myNewArrayFilter = myArrayFilter.filter((num) => {
+  return num % 2 === 0;
+});
+
+console.log(myNewArrayFilter);
